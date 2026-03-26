07734c1f041662c2f5189a3aac8923fbfceaa104
Jack <haujack_pang@yahoo.com>
2026-03-26 22:29:09 +0800

    Receipt upload: Supabase Storage + file picker in WebView

--- android-app/app/src/main/java/com/homestay/expense/MainActivity.java
+++ android-app/app/src/main/java/com/homestay/expense/MainActivity.java
@@ -1,12 +1,14 @@
 package com.homestay.expense;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -15,6 +17,8 @@ import android.webkit.WebViewClient;
 public class MainActivity extends Activity {
 
     private WebView webView;
+    private ValueCallback<Uri[]> fileCallback;
+    private static final int FILE_CHOOSER_CODE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +53,41 @@ public class MainActivity extends Activity {
         }
 
         webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(WebView view, ValueCallback<Uri[]> callback,
+                                             FileChooserParams params) {
+                if (fileCallback != null) fileCallback.onReceiveValue(null);
+                fileCallback = callback;
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("*/*");
+                intent.putExtra(Intent.EXTRA_MIME_TYPES, new String[]{"image/*", "application/pdf"});
+
+                Intent chooser = Intent.createChooser(intent, "Select receipt");
+                startActivityForResult(chooser, FILE_CHOOSER_CODE);
+                return true;
+            }
+        });
 
         webView.loadUrl("file:///android_asset/home_expense.htm");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == FILE_CHOOSER_CODE) {
+            if (fileCallback != null) {
+                Uri[] results = null;
+                if (resultCode == RESULT_OK && data != null) {
+                    Uri uri = data.getData();
+                    if (uri != null) results = new Uri[]{uri};
+                }
+                fileCallback.onReceiveValue(results);
+                fileCallback = null;
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
